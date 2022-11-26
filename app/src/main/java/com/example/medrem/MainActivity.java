a9272e935e1a835bc4bb4ac6c753d6e85307099f
236502 Bednarski Kacper <236502@edu.p.lodz.pl>
2022-11-26 19:40:57 +0100

    Bottom navigation menu created

--- app/src/main/java/com/example/medrem/MainActivity.java
+++ app/src/main/java/com/example/medrem/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.medrem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import com.example.medrem.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new TodayFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.today:
+                    replaceFragment(new TodayFragment());
+                    break;
+                case R.id.progress:
+                    replaceFragment(new ProgressFragment());
+                    break;
+                case R.id.therapy:
+                    replaceFragment(new TherapyFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
