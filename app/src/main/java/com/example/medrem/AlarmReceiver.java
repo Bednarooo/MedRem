fa7719d9a3df65bb58ad61d8d35bd30ec2a1dda9
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-12-14 00:27:21 +0100

    notifications to medicine works

--- app/src/main/java/com/example/medrem/AlarmReceiver.java
+++ app/src/main/java/com/example/medrem/AlarmReceiver.java
@@ -1,13 +1,20 @@
 package com.example.medrem;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class AlarmReceiver extends BroadcastReceiver {
+    private NotificationManagerCompat notificationManager = null;
+    private static final String CHANNEL_ID = "SampleChannel";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         int notificationId = intent.getIntExtra("notificationId", 0);
@@ -16,15 +23,22 @@ public class AlarmReceiver extends BroadcastReceiver {
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setSmallIcon(android.R.drawable.ic_dialog_alert)
-                .setContentTitle("It's time!")
+
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence channel_name = "MedicineNotification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channel_name, importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle("Get your medicine!")
                 .setContentText(message)
-                .setWhen(System.currentTimeMillis())
-                .setAutoCancel(true)
-                .setContentIntent(contentIntent);
+                .setContentIntent(contentIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
 
         notificationManager.notify(notificationId, builder.build());
-
     }
 }
